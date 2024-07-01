326092a27628a9fa97462f5188cc31cdfd09ea71
Megha <meghamokashi193@gmail.com>
2024-07-01 10:37:13 +0530

    size changed

--- src/contact-lens/WebcamFeed.js
+++ src/contact-lens/WebcamFeed.js
@@ -220,7 +220,7 @@ const WebcamFeed = () => {
       const irisSize = Math.min(irisWidth, irisHeight) * 1.6;
 
       // Set transparency level
-      canvasCtx.globalAlpha = 0.18;
+      canvasCtx.globalAlpha = 0.25;
 
       canvasRef.current.style.filter = "blur(2px)";
       // Draw the lens
